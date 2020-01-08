f0ec3402372c8073d56c5df85cfc5166a7e134ec
Maor Rocky <maorrocky@gmail.com>
2020-01-08 22:19:35 +0200

    Add files via upload

--- code-gen.ml
+++ code-gen.ml
@@ -11,21 +11,21 @@ module type CODE_GEN = sig
        * the offset from the base const_table address in bytes; and
        * a string containing the byte representation (or a sequence of nasm macros)
          of the constant value
-     For example: [(Sexpr(Nil), (1, "SOB_NIL"))]
-   *)
+       For example: [(Sexpr(Nil), (1, "SOB_NIL"))]
+  *)
   val make_consts_tbl : expr' list -> (constant * (int * string)) list
 
   (* This signature assumes the structure of the fvars table is
      a list of key-value pairs:
      - The keys are the fvar names as strings
      - The values are the offsets from the base fvars_table address in bytes
-     For example: [("boolean?", 0)]
-   *)  
+       For example: [("boolean?", 0)]
+  *)  
   val make_fvars_tbl : expr' list -> (string * int) list
 
   (* This signature represents the idea of outputing assembly code as a string
      for a single AST', given the full constants and fvars tables. 
-   *)
+  *)
   val generate : (constant * (int * string)) list -> (string * int) list -> expr' -> string
 end;;
 
@@ -65,7 +65,7 @@ let rec get_all_sexps expr'=
   let rec func expr' = 
     match expr' with
     | Const'(Sexpr(sexp)) -> (let a = global_lst := List.append !global_lst [sexp] in
-                       just_ret_unit a)
+                              just_ret_unit a)
     | Const'(Void) -> ()
     | Var'(var) -> ()
     | Box'(var) -> ()
@@ -84,10 +84,10 @@ let rec get_all_sexps expr'=
     | ApplicTP'(operator, args) -> (let a = [func operator] in
                                     let b = List.map func args in
                                     just_ret_unit [a; b]) in
-    
+
   let just_a_unit = func expr' in
   car !global_lst just_a_unit
-  ;;
+;;
 
 let unique_cons xs x = 
   if List.mem x xs 
@@ -117,19 +117,19 @@ let rec from_sexp_lst_to_const_tbl lst =
                         (Sexpr(Bool(false)), (2, "MAKE_BOOL(0)")); (Sexpr(Bool(true)), (4, "MAKE_BOOL(1)"))] in
   let add_next_tuple sexp =
     let rec get_offset_of_sexp sexp const_tbl = 
-        match const_tbl with
-        | (Sexpr(sexpr), (offset, representation)) :: rest when (sexpr_eq sexpr sexp) -> offset
-        | tuple :: rest -> get_offset_of_sexp sexp rest
-        | [] -> (-1) in
+      match const_tbl with
+      | (Sexpr(sexpr), (offset, representation)) :: rest when (sexpr_eq sexpr sexp) -> offset
+      | tuple :: rest -> get_offset_of_sexp sexp rest
+      | [] -> (-1) in
     let get_next_offset = 
-        let last_tuple = List.nth !global_lst ((List.length !global_lst) - 1) in
-        match last_tuple with
-        | (Sexpr(Bool(_)), (offset, representation)) -> offset + 2
-        | (Sexpr(Char(_)), (offset, representation)) -> offset + 2
-        | (Sexpr(Number(_)), (offset, representation)) -> offset + 9
-        | (Sexpr(String(str)), (offset, representation)) -> offset + 9 + (String.length str)
-        | (Sexpr(Symbol(str)), (offset, representation)) -> offset + 9
-        | (Sexpr(Pair(car, cdr)), (offset, representation)) -> offset + 17 in
+      let last_tuple = List.nth !global_lst ((List.length !global_lst) - 1) in
+      match last_tuple with
+      | (Sexpr(Bool(_)), (offset, representation)) -> offset + 2
+      | (Sexpr(Char(_)), (offset, representation)) -> offset + 2
+      | (Sexpr(Number(_)), (offset, representation)) -> offset + 9
+      | (Sexpr(String(str)), (offset, representation)) -> offset + 9 + (String.length str)
+      | (Sexpr(Symbol(str)), (offset, representation)) -> offset + 9
+      | (Sexpr(Pair(car, cdr)), (offset, representation)) -> offset + 17 in
     let offset = (get_next_offset) in
     match sexp with
     | Bool(true) -> (global_lst := !global_lst)
@@ -138,15 +138,15 @@ let rec from_sexp_lst_to_const_tbl lst =
     | Number(Int(num)) -> (global_lst := List.append !global_lst [(Sexpr(sexp), (offset, "MAKE_LITERAL_INT("^string_of_int num^")"))])
     | Number(Float(num)) -> (global_lst := List.append !global_lst [(Sexpr(sexp), (offset, "MAKE_LITERAL_INT("^string_of_float num^")"))])
     | Char(ch) -> (global_lst := List.append !global_lst 
-                                        [(Sexpr(sexp), (offset, "MAKE_LITERAL_CHAR("^string_of_int(int_of_char(ch))^")"))])
+                       [(Sexpr(sexp), (offset, "MAKE_LITERAL_CHAR("^string_of_int(int_of_char(ch))^")"))])
     | String(str) -> (global_lst := List.append !global_lst [(Sexpr(sexp), (offset, "MAKE_LITERAL_STRING("^str^")"))])
     | Symbol(str) -> (let symbol_offset = get_offset_of_sexp (String(str)) !global_lst in
-                             global_lst := List.append !global_lst 
-                                           [(Sexpr(sexp), (offset, "MAKE_LITERAL_SYMBOL(consts+"^string_of_int(symbol_offset)^")"))])
+                      global_lst := List.append !global_lst 
+                          [(Sexpr(sexp), (offset, "MAKE_LITERAL_SYMBOL(consts+"^string_of_int(symbol_offset)^")"))])
     | Pair(car, cdr) -> (let car_offset = string_of_int(get_offset_of_sexp car !global_lst) in
-                                let cdr_offset = string_of_int(get_offset_of_sexp cdr !global_lst) in
-                                global_lst := List.append !global_lst 
-                                            [(Sexpr(sexp), (offset, "MAKE_LITERAL_PAIR(consts+"^car_offset^", consts+"^cdr_offset^")"))]) in
+                         let cdr_offset = string_of_int(get_offset_of_sexp cdr !global_lst) in
+                         global_lst := List.append !global_lst 
+                             [(Sexpr(sexp), (offset, "MAKE_LITERAL_PAIR(consts+"^car_offset^", consts+"^cdr_offset^")"))]) in
   let dont_care = List.map add_next_tuple lst in
   car !global_lst dont_care;;
 
@@ -157,14 +157,16 @@ let check_tags = fun str -> rename_tags_all_asts (my_runs (my_tags (my_read_sexp
 (* get_all_sexps (boxing (tail_cals false (lexical_addresses [] [] false ()))) *)
 
 
-(*let rec get_all_freeVars expr'=
+let rec get_all_freeVars expr'=
   let global_lst = ref [] in
   let rec func expr' = 
     match expr' with
-    | Const' (Sexpr(a))-> []
-    | Var'(VarFree(var)) -> [var]
+    | Var'(VarFree(var)) -> (let a = global_lst := List.append !global_lst [var] in
+                             just_ret_unit a)
+    | Const'(Sexpr(sexp)) -> ()
+    | Const'(Void) -> ()
     | Box'(var) -> ()
-    (* | BoxGet'(var) -> () *)
+    | BoxGet'(var) -> ()
     | BoxSet'(var, expr) -> (func expr)
     | If'(test, dit, dif) -> (just_ret_unit (List.map func [test; dit; dif])) 
     | Seq'(exprs) -> (just_ret_unit (List.map func exprs))
@@ -179,25 +181,112 @@ let check_tags = fun str -> rename_tags_all_asts (my_runs (my_tags (my_read_sexp
     | ApplicTP'(operator, args) -> (let a = [func operator] in
                                     let b = List.map func args in
                                     just_ret_unit [a; b]) in
+
   let just_a_unit = func expr' in
   car !global_lst just_a_unit
-  ;;
+;;
 
 let create_free_vars_table asts =
-  let initial_fvars =["boolean?",(0, "boolean?"); "float?",(1,"float?"); "integer?",(2,"integer?");
-  "pair?",(3,"pair?");
-  "null?",(4,"null?"); "char?",(5,"char?"); "vector?",(6,"vector?"); "string?",(7,"string?");
-  "procedure?",(8,"procedure?"); "symbol?", (9,"symbol?"); "string-length", (10, "string-length") ;
-  "string-ref",(11, "string-ref") ; "string-set!",(12, "string-set!"); "make-string",(13,"make-string");
-  "vector-length", (14, "vector-length"); "vector-ref",(15,"vector-ref"); "vector-set!",(16,"vector-set!");
-  "make-vector", (17,"make-vector"); "symbol->string",(18,"symbol->string");
-  "char->integer", (19, "char->integer"); "integer->char", (20,"integer->char"); "eq?", (21, "eq?");
-  "+",(22,"+"); "*",(23,"*"); "-",(24,"-"); "/",(25,"/"); "<", (26,"<" ); "=",(27,"=");
-  "car", (28,"car"); "cdr",(29,"cdr");"set-car!",(30,"set-car!");"set-cdr!",(31,"set-cdr!");
-  "apply", (32, "apply");"cons",(33,"cons");
-(*  "bin_add"; "*", "bin_mul"; "-", "bin_sub"; "/", "bin_div"; "<", "bin_lt"; "=", "bin_equ"*)] in
-  asts
-  ;; *)
+  let initial_fvars =[("boolean?",0); ("float?",1); ("integer?",2);
+                      ("pair?",3);("null?",4);("char?",5); ("vector?",6); ("string?",7);
+                      ("procedure?",8); ("symbol?",9);  ("string-length",10) ;
+                      ("string-ref",11) ; ( "string-set!",12); ("make-string",13);
+                      ("vector-length",14); ("vector-ref",15); ("vector-set!",16);
+                      ("make-vector",17);("symbol->string",18);("char->integer",19); 
+                      ("integer->char",20);  ("eq?",21);
+                      ("+",22); ("*",23); ("-",24); ("/",25); ("<",26 ); ("=",27);
+                      ("car",28); ("cdr",29);("set-car!",30);("set-cdr!",31); ("apply",32);("cons",33);
+                     ] in
+  let rec add_fvar_tuple fvars_to_add fvar_table index=
+    if List.length fvars_to_add < 1 (* Break *)
+    then fvar_table
+    else(
+      let head_fvar = List.hd fvars_to_add in
+      let fvar_table_names = List.map (fun (name,_)-> name) fvar_table in
+      if List.mem head_fvar fvar_table_names (* no need to add this fvar*)
+      then (add_fvar_tuple (List.tl fvars_to_add) fvar_table index) (* continue *)
+      else (* adding the fvar *)
+        (let x = (head_fvar,index) in
+         add_fvar_tuple (List.tl fvars_to_add) (List.append fvar_table [x]) (index + 1)))
+  in
+  let fvars_from_ast = (remove_dups_from_left(get_all_freeVars asts)) in
+  (add_fvar_tuple fvars_from_ast initial_fvars 34);;
+;; 
+(* find index of free var in fvars_table *)
+let rec getIndex_fvars fvars_table fvar_name =
+  if List.length fvars_table < 1 
+  then -1
+  else
+    let (name,fvar_index) = (List.hd fvars_table) in
+    if(String.equal name fvar_name) 
+    then fvar_index
+    else getIndex_fvars (List.tl fvars_table) fvar_name ;;
+
+let getIndex_const_table const const_table =
+  let (sexp,(index,str)) =  
+    (List.find (fun (sexp,(address,str)) -> sexpr_eq sexp const) const_table) in
+  index;;
+
+(* global index *)
+let index = ref 0;;
+(* the main method 
+   im not sure about the paremeters of the function*)
+let increment_index =
+  fun () -> index := !index + 1; !index - 1;;
+let  generate consts fvars e  =
+  (* will be used to help us with applic *)
+  (* let rec applic_helper lst i =
+     if List.length lst < 1 
+     then ""
+     else
+      (mainGenerate (List.hd lst) i )^"push rax\n"^(applic_helper (List.tl lst) i) 
+
+  *)
+  let rec or_helper lst e' ind env =
+    if List.length lst < 1 
+    then "Lexit"^(string_of_int ind)^":\n" 
+    else
+    if  List.length lst = 1 
+    then (mainGenerate (List.hd lst) env)^(or_helper( (List.tl lst)) e' ind env)
+    else
+      (mainGenerate (List.hd lst) env)^"cmp rax, sob_false\njne Lexit"^(string_of_int ind)^"\n"^(or_helper ((List.tl lst)) e' ind env)
+  and if_helper test dit dif env ind =
+    (mainGenerate test env) ^ "\n" ^
+    "cmp rax, SOB_FALSE_ADDRESS\n" ^
+    "je Lelse_" ^ (string_of_int ind) ^ "\n" ^  
+    (mainGenerate dit env) ^ "\n" ^
+    "jmp Lexit_" ^ (string_of_int ind) ^ "\n" ^ 
+    "Lelse_" ^ (string_of_int ind) ^ ":\n" ^
+    (mainGenerate dif env) ^ "\n" ^
+    "Lexit_" ^ (string_of_int ind) ^ ":\n"
+
+  and mainGenerate e env = 
+    match e with        
+    | Const'(Sexpr(c)) -> let i = (getIndex_const_table c consts) in "mov rax, const_tbl+"^(string_of_int i)^"\n"
+    (* NADAV: WHAT SHOULD WE DO IF INDEX IS -1? *)
+    | Var'(VarParam(_, minor)) -> "mov qword rax, [rbp + "^(string_of_int (8*(minor+4)))^"]\n"
+    | Set'(Var'(VarParam(_, minor)), e') ->   (* NOT THE SAME AS e as e' *)
+      (mainGenerate e' env) ^"mov qword [rbp + "^(string_of_int (8*(4+minor)))^"], rax\nmov rax, sob_void\n"
+    | Var'(VarBound(_, major, minor)) ->
+      "mov rax, qword [rbp + 16]\n"^
+      "mov rax, qword [rax + "^(string_of_int (major*8))^"]\n"^
+      "mov rax, qword [rax + "^(string_of_int (minor*8))^"]\n"
+    | Set'(Var'(VarBound(_, major, minor)), e') ->
+      (mainGenerate e' env)^
+      "mov rbx, qword [rbp + 8 ∗ 2]\n"^
+      "mov rbx, qword [rbx + 8 ∗"^(string_of_int major)^"]\n"^
+      "mov qword [rbx + 8 ∗"^(string_of_int minor)^"], rax\n"^
+      "mov rax, sob_void\n"
+    | Set'(Var'(VarFree(s)), e') -> (mainGenerate e' env)^
+                                    "mov qword [fvar_tbl+"^(string_of_int (8*(getIndex_fvars fvars s)))^"], rax\nmov rax, sob_void\n"
+    | Var'(VarFree(v)) -> "mov rax, qword [fvar_tbl + "^(string_of_int (8*(getIndex_fvars fvars v)))^"]\n"
+    |Seq' (lst) -> List.fold_left (fun acc curr -> acc ^ "\n" ^ (mainGenerate curr env) ^ "\n") "" lst 
+    | Or'(lst) -> let ind = increment_index () in (or_helper lst e ind env)
+    |If' (test, dit, dif) -> 
+      let ind = increment_index () in
+      (if_helper test dit dif env ind)
+
+  in mainGenerate e 0;;
 
 module Code_Gen : CODE_GEN = struct
   let make_consts_tbl asts = raise X_not_yet_implemented;;
