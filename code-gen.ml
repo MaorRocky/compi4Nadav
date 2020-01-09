5b71c8482d74afaaf7bd8a1b8576370e76837f58
Maor Rocky <maorrocky@gmail.com>
2020-01-09 17:02:19 +0200

    Add files via upload

--- code-gen.ml
+++ code-gen.ml
@@ -206,8 +206,8 @@ let create_free_vars_table asts =
       if List.mem head_fvar fvar_table_names (* no need to add this fvar*)
       then (add_fvar_tuple (List.tl fvars_to_add) fvar_table index) (* continue *)
       else (* adding the fvar *)
-        (let x = (head_fvar,index) in
-         add_fvar_tuple (List.tl fvars_to_add) (List.append fvar_table [x]) (index + 1)))
+        (let newFvarTuple = (head_fvar,index) in
+         add_fvar_tuple (List.tl fvars_to_add) (List.append fvar_table [newFvarTuple]) (index + 1)))
   in
   let fvars_from_ast = (remove_dups_from_left(get_all_freeVars asts)) in
   (add_fvar_tuple fvars_from_ast initial_fvars 34);;
@@ -221,7 +221,7 @@ let rec getIndex_fvars fvars_table fvar_name =
     if(String.equal name fvar_name) 
     then fvar_index
     else getIndex_fvars (List.tl fvars_table) fvar_name ;;
-
+(* NADAV you might need to change it im not sure how const table is set up *)
 let getIndex_const_table const const_table =
   let (sexp,(index,str)) =  
     (List.find (fun (sexp,(address,str)) -> sexpr_eq sexp const) const_table) in
@@ -229,10 +229,11 @@ let getIndex_const_table const const_table =
 
 (* global index *)
 let index = ref 0;;
-(* the main method 
-   im not sure about the paremeters of the function*)
+
 let increment_index =
   fun () -> index := !index + 1; !index - 1;;
+(* the main method 
+   im not sure about the paremeters of the function*)
 let  generate consts fvars e  =
   (* will be used to help us with applic *)
   (* let rec applic_helper lst i =
@@ -261,39 +262,94 @@ let  generate consts fvars e  =
     (mainGenerate dif env) ^ "\n" ^
     "Lexit_" ^ (string_of_int ind) ^ ":\n"
 
-  and mainGenerate e env = 
-    match e with        
-    | Const'(Sexpr(c)) -> let i = (getIndex_const_table c consts) in "mov rax, const_tbl+"^(string_of_int i)^"\n"
-    (* NADAV: WHAT SHOULD WE DO IF INDEX IS -1? *)
-    | Var'(VarParam(_, minor)) -> "mov qword rax, [rbp + "^(string_of_int (8*(minor+4)))^"]\n"
-    | Set'(Var'(VarParam(_, minor)), e') ->   (* NOT THE SAME AS e as e' *)
-      (mainGenerate e' env) ^"mov qword [rbp + "^(string_of_int (8*(4+minor)))^"], rax\nmov rax, sob_void\n"
-    | Var'(VarBound(_, major, minor)) ->
-      "mov rax, qword [rbp + 16]\n"^
-      "mov rax, qword [rax + "^(string_of_int (major*8))^"]\n"^
-      "mov rax, qword [rax + "^(string_of_int (minor*8))^"]\n"
-    | Set'(Var'(VarBound(_, major, minor)), e') ->
-      (mainGenerate e' env)^
-      "mov rbx, qword [rbp + 8 ∗ 2]\n"^
-      "mov rbx, qword [rbx + 8 ∗"^(string_of_int major)^"]\n"^
-      "mov qword [rbx + 8 ∗"^(string_of_int minor)^"], rax\n"^
-      "mov rax, sob_void\n"
-    | Set'(Var'(VarFree(s)), e') -> (mainGenerate e' env)^
-                                    "mov qword [fvar_tbl+"^(string_of_int (8*(getIndex_fvars fvars s)))^"], rax\nmov rax, sob_void\n"
-    | Var'(VarFree(v)) -> "mov rax, qword [fvar_tbl + "^(string_of_int (8*(getIndex_fvars fvars v)))^"]\n"
-    |Seq' (lst) -> List.fold_left (fun acc curr -> acc ^ "\n" ^ (mainGenerate curr env) ^ "\n") "" lst 
-    | Or'(lst) -> let ind = increment_index () in (or_helper lst e ind env)
-    |If' (test, dit, dif) -> 
-      let ind = increment_index () in
-      (if_helper test dit dif env ind)
-    |BoxGet' (v) -> (mainGenerate (Var'(v)) env)  ^ "\n" ^
-                    "mov rax, qword [rax]\n"
-    |BoxSet' (v, value) -> (mainGenerate value env) ^ "\n" ^
-                           "push rax\n" ^
-                           (mainGenerate (Var'(v)) env) ^ "\n" ^
-                           "pop qword [rax]\n" ^ 
-                           "mov rax, sob_void\n"
-  in mainGenerate e 0;;
+  and lambda_helper params body env = (*  NADAV PUT COMMENTS *)
+    let ind = increment_index() in
+    let extend = 
+      if env < 1
+      then   
+        "MAKE_CLOSURE(rax, SOB_NIL_ADDRESS, Lcode"^(string_of_int ind)^")\n
+        jmp Lcont"^(string_of_int ind)^"\n
+        Lcode"^(string_of_int ind)^":\n
+        push rbp\n
+        mov rbp, rsp\n
+        "^(mainGenerate body (env+1))^"
+        leave\n
+        ret\n
+        Lcont"^(string_of_int ind)^":\n"  
+      else
+        "MALLOC rbx, "^(string_of_int env)^" * WORD_SIZE
+        mov qword rcx, [rbp+16] ; now rcx holds a pointer to the env
+        mov rdx, "^(string_of_int env)^" ; depth of the env is now in rdx, will be used in loop counter
+        dec rdx
+        mov r10, 0 ; will use r10 to compare to 0
+        mov r9,1 ; will use r9 to multiply by 8
+        .loop: 
+          cmp r10, rdx
+          je .loop_end
+          mov qword r11, [rcx+8*r9]
+          mov qword [rbx+8*r9], r11
+          inc r9
+          dec rdx
+          jmp .loop
+        .loop_end:
+          mov qword r8, [rbp+24] ; r8 hold the args count
+          shl r8 3 ; n:=n*8 
+          MALLOC rax, r8
+          mov qword [rbx], rax 
+          mov rdx, r8 ; rdx holds the args count * 8, will be used in the loop
+        .create_loop:
+          cmp r10, rdx
+          je jmp .end_create_loop
+          mov qword r12, [rbp + 8 * (4+r10)]
+          mov qword [rax + **r10], r12
+          inc r10
+          jmp .create_loop
+        .end_create_loop:
+        MAKE_CLOSURE(rax, rbx, Lcode"^(string_of_int ind)^")
+        jmp Lcont"^(string_of_int ind)^"
+        Lcode"^(string_of_int ind)^":
+        push rbp
+        mov rbp, rsp
+        "^(mainGenerate body (env+1))^"
+        leave
+        ret
+        Lcont"^(string_of_int ind)^":\n"
+
+
+    and mainGenerate e env = 
+      match e with        
+      | Const'(Sexpr(c)) -> let i = (getIndex_const_table c consts) in "mov rax, const_tbl+"^(string_of_int i)^"\n"
+      (* NADAV: WHAT SHOULD WE DO IF INDEX IS -1? *)
+      | Var'(VarParam(_, minor)) -> "mov qword rax, [rbp + "^(string_of_int (8*(minor+4)))^"]\n"
+      | Set'(Var'(VarParam(_, minor)), e') ->   (* NOT THE SAME AS e as e' *)
+        (mainGenerate e' env) ^"mov qword [rbp + "^(string_of_int (8*(4+minor)))^"], rax\nmov rax, sob_void\n"
+      | Var'(VarBound(_, major, minor)) ->
+        "mov rax, qword [rbp + 16]\n"^
+        "mov rax, qword [rax + "^(string_of_int (major*8))^"]\n"^
+        "mov rax, qword [rax + "^(string_of_int (minor*8))^"]\n"
+      | Set'(Var'(VarBound(_, major, minor)), e') ->
+        (mainGenerate e' env)^
+        "mov rbx, qword [rbp + 8 ∗ 2]\n"^
+        "mov rbx, qword [rbx + 8 ∗"^(string_of_int major)^"]\n"^
+        "mov qword [rbx + 8 ∗"^(string_of_int minor)^"], rax\n"^
+        "mov rax, sob_void\n"
+      | Set'(Var'(VarFree(s)), e') -> (mainGenerate e' env)^
+                                      "mov qword [fvar_tbl+"^(string_of_int (8*(getIndex_fvars fvars s)))^"], rax\nmov rax, sob_void\n"
+      |Var'(VarFree(v)) -> "mov rax, qword [fvar_tbl + "^(string_of_int (8*(getIndex_fvars fvars v)))^"]\n"
+      |Seq' (lst) -> List.fold_left (fun acc curr -> acc ^ "\n" ^ (mainGenerate curr env) ^ "\n") "" lst 
+      |Or'(lst) -> let ind = increment_index () in (or_helper lst e ind env)
+      |If' (test, dit, dif) -> 
+        let ind = increment_index () in
+        (if_helper test dit dif env ind)
+      |BoxGet' (v) -> (mainGenerate (Var'(v)) env)  ^ "\n" ^
+                      "mov rax, qword [rax]\n"
+      |BoxSet' (v, e') -> (mainGenerate e' env) ^ "\n" ^
+                          "push rax\n" ^
+                          (mainGenerate (Var'(v)) env) ^ "\n" ^
+                          "pop qword [rax]\n" ^ 
+                          "mov rax, sob_void\n"
+      |LambdaSimple'(params, body) -> lambda_helper params body env
+    in mainGenerate e 0;;
 
 module Code_Gen : CODE_GEN = struct
   let make_consts_tbl asts = raise X_not_yet_implemented;;
